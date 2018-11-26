810144f36b5f881ce9438b24fd4c63e9c99ec61c
sachablade <juanma.egm31@gmail.com>
2018-11-26 23:22:23 +0100

    initial

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import style from "./main.css";
+
+
+const App = () => {
+  return (
+    <div>
+      <p>React here!</p>
+    </div>
+  );
+};
+export default App;
+ReactDOM.render(<App />, document.getElementById("app"));
